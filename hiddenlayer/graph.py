6d7546c513a6f851022602670d5834f0a8bc18f3
Jiazhen Xie <fdf515@gmail.com>
2020-01-05 17:49:46 +0000

    Retry

--- hiddenlayer/graph.py
+++ hiddenlayer/graph.py
@@ -324,6 +324,7 @@ class Graph():
                  fontcolor=self.theme["font_color"],
                  fontname=self.theme["font_name"],
                  margin=self.theme["margin"],
+                 rankdir="TB",
                  pad=self.theme["padding"])
         dot.attr("node", shape="box",
                  style="filled", margin="0,0",
